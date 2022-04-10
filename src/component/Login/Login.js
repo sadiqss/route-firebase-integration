271004397c9eb19ecf00ced355735faeaa0da462
sadiqss <sadiq.sau48@gmail.com>
2022-04-10 03:16:06 +0100

    firebase hook

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h2>Please Login</h2>
+            <div style={{ margin: '15px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+            <form>
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input type="password" placeholder='password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
